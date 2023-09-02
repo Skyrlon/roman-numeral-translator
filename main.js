b9d1142b141f4067fd3c3dea32f145a15b56f8fa
Antoine Drouault <65726981+Skyrlon@users.noreply.github.com>
2023-09-02 23:31:30 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const romanNumeralInput = document.querySelector("#numeral-input");
+const buttonElement = document.querySelector("#submit-button");
+
+const romanNumeralList = ["M", "D", "C", "L", "X", "V", "I"];
+
+buttonElement.addEventListener("click", translateNumber);
+
+function translateNumber() {
+    
+}
