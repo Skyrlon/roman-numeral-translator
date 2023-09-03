76c0f817ba3b50f15d9418ea2f9035c0d547ff76
Antoine Drouault <antoinedrouault@hotmail.com>
2023-09-03 23:43:54 +0200

    Make translation for simple roman numeral

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-const romanNumeralInput = document.querySelector("#numeral-input");
+const romanNumeralInput = document.querySelector("#roman-numeral");
 const buttonElement = document.querySelector("#submit-button");
+const resultElement = document.querySelector("#result");
 
-const romanNumeralList = ["M", "D", "C", "L", "X", "V", "I"];
+const numeralsList = [
+  { roman: "M", arab: 1000 },
+  { roman: "D", arab: 500 },
+  { roman: "C", arab: 100 },
+  { roman: "L", arab: 50 },
+  { roman: "X", arab: 10 },
+  { roman: "V", arab: 5 },
+  { roman: "I", arab: 1 },
+];
 
-buttonElement.addEventListener("click", translateNumber);
+buttonElement.addEventListener("click", function () {
+  translateNumber(romanNumeralInput.value);
+});
 
-function translateNumber() {
-    
+function translateNumber(number) {
+  let translatedNumber = 0;
+  number
+    .split("")
+    .forEach(
+      (e) =>
+        (translatedNumber =
+          translatedNumber + numeralsList.find((x) => x.roman === e).arab)
+    );
+  resultElement.textContent = translatedNumber;
 }
